df8ae0c68d6bd4aabd2a5a72d65b8f8acdc97a79
ziggsmachine <toto@waveit.ar>
2024-01-19 23:29:49 -0300

    poniendo fachero el form

--- api/models.py
+++ api/models.py
@@ -7,3 +7,5 @@ class User(models.Model):
     fecha_de_entrada = models.DateField(default="0-0-0")
     numero_telefono = models.CharField(max_length=100, default="Telefono no establecido")
     dias_abonados = models.IntegerField(default=0)
+    nombre_completo = models.CharField(max_length=255, default="Nombre no establecido")
+    direccion = models.CharField(max_length=255, default="Direccion no establecida")
